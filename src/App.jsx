04de4447203cd83b78fc9c0e919cbe1bff3a6a20
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-06-25 14:06:58 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import { Heading } from 'components';
+import { Route, Routes } from 'react-router-dom';
 
-export const App = () => {
-  return <Heading title="App" bottom />;
+import Country from 'pages/Country';
+import Home from 'pages/Home';
+import SearchCountry from 'pages/SearchCountry';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/country" element={<Country />} />
+      <Route path="/country/:countryId" element={<SearchCountry />} />
+    </Routes>
+  );
 };
+
+export default App;
